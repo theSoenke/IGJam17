554e3e6c7aac7be456210745c2820b7fc880f328
Unknown <nilsw.agner@web.de>
2017-10-28 11:59:41 +0200

    player controller with movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region exposed fields
+    [SerializeField]
+    private float _movementSpeed = 1.0f;
+    [SerializeField]
+    private string _inputAxisX = "Horizontal";
+    [SerializeField]
+    private string _inputAxisY = "Vertical";
+    #endregion
+
+    private Rigidbody2D _rigidbody;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        var inputVector = new Vector3(Input.GetAxis(_inputAxisX), Input.GetAxis(_inputAxisY));
+
+        var speedVector = inputVector * _movementSpeed;
+
+        var positionDelta = speedVector * Time.deltaTime;
+
+        _rigidbody.velocity = speedVector;
+
+        //transform.position += positionDelta;
+
+    }
+}
